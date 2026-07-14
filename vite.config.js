6e54b50fdb47312f66b104590fe63f544e9809b2
Camilla Westin <wesca@devcore.se>
2026-07-14 11:36:33 +0200

    Configure GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import process from 'node:process'
+
+const repositoryName = process.env.GITHUB_REPOSITORY?.split('/')[1]
+const isUserOrOrgPage = repositoryName?.endsWith('.github.io')
+const base = process.env.GITHUB_ACTIONS && repositoryName && !isUserOrOrgPage
+  ? `/${repositoryName}/`
+  : '/'
 
 export default defineConfig({
+  base,
   plugins: [react()],
 })
